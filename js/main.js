f48d73545c294fcfddae27482fee77187e9e697c
alok <rajalok23112002@gmail.com>
2022-04-18 23:00:51 +0530

    Initial

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function togglehide1(){
+    let first = document.getElementById('l') 
+    let content1 = document.getElementById('1')
+    let content2 = document.getElementById('2')
+    let content3 = document.getElementById('3')
+    let content4 = document.getElementById('l')
+    let content5 = document.getElementById('m')
+    let content6 = document.getElementById('n')
+    content1.style.display = "block";
+    content2.style.display = "none";
+    content3.style.display = "none";
+    content4.style.color = " #ff511a";
+    content5.style.color ="black";
+    content6.style.color ="black"
+}
+function togglehide2(){
+    let content1 = document.getElementById('1')
+    let content2 = document.getElementById('2')
+    let content3 = document.getElementById('3')
+    let content4 = document.getElementById('l')
+    let content5 = document.getElementById('m')
+    let content6 = document.getElementById('n')
+    content1.style.display = "none";
+    content2.style.display = "block";
+    content3.style.display = "none";
+    content4.style.color = " black";
+    content5.style.color ="#ff511a";
+    content6.style.color ="black"
+}
+
+function togglehide3(){
+    let content1 = document.getElementById('1')
+    let content2 = document.getElementById('2')
+    let content3 = document.getElementById('3')
+    let content4 = document.getElementById('l')
+    let content5 = document.getElementById('m')
+    let content6 = document.getElementById('n')
+    content1.style.display = "none";
+    content2.style.display = "none";
+    content3.style.display = "block";
+    content4.style.color = "black";
+    content5.style.color ="black";
+    content6.style.color =" #ff511a"
+}
